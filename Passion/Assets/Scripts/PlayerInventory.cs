35f1d102d9e44b1c16e4d40a7f5af05a199d1b67
Kenzo-dewaegenaere <71668365+Kenzo-dewaegenaere@users.noreply.github.com>
2021-11-14 17:53:53 +0100

    #9 Pick up, up done need to store the item now

--- Passion/Assets/Scripts/PlayerInventory.cs
+++ Passion/Assets/Scripts/PlayerInventory.cs
@@ -8,12 +8,16 @@ public class PlayerInventory : MonoBehaviour
     public GameObject inventoryUI;
     void Update()
     {
+        
 
+        //open the inventory on I press
         if (Input.GetKeyDown(KeyCode.I))
             {
                 inventoryUI.SetActive(true);
              }
 
+
+        //close the inventory on O press
         if (Input.GetKeyDown(KeyCode.O))
         {
             inventoryUI.SetActive(false);
