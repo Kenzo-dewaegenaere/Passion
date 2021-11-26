3a5971e57c88d2d609b16dea46d8dfb487a74d54
Kenzo-dewaegenaere <71668365+Kenzo-dewaegenaere@users.noreply.github.com>
2021-11-26 19:28:52 +0100

    #29 trying to switch shaders in runtime

---
+++ PassionProject/Assets/Scripts/MaterialStateController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialStateController : MonoBehaviour
+{
+
+    public Material[] material;
+    public int x;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0;
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[x];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.sharedMaterial = material[x];
+    }
+
+
+    public void NextColor()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            x++;
+        }
+        else if(Input.GetMouseButtonDown(1))
+        {
+            x = 0;
+        }
+    }
+}
