6de10733ca15856d8ee2ae0fd7d23de12d2b2abd
Kenzo-dewaegenaere <71668365+Kenzo-dewaegenaere@users.noreply.github.com>
2021-12-05 14:36:26 +0100

    #27 further integration, will build first level

--- PassionProject/Assets/Shaders/CustomLighting.hlsl
+++ PassionProject/Assets/Shaders/CustomLighting.hlsl
@@ -1,18 +1,21 @@
-void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten) {
-
-#ifdef SHADERGRAPH_OREVIEW
-Direction = float3(0.5, 0.5, 0);
-Color = 1;
-DistanceAtten = 1;
-ShadowAtten  =1;
-#else 
-Light light = GetLight();
-Direction = light.direction; 
-Color = light.color;
-DistanceAtten = light.attenuation; 
-float4 shadowCoord = TransformWorld1ToShadowCoord(WorldPos); 
-ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
-half shadowStrength = GetMainLightShadowStrength();
-ShadowAtten = SampleShadowMap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowMapTexture, sampler_MainLightShadowMapTexture), shadowSamplingData, shadowStrength, false);
-#endif 
+void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
+{
+#ifdef SHADERGRAPH_PREVIEW
+   Direction = half3(0.5, 0.5, 0);
+   Color = 1;
+   DistanceAtten = 1;
+   ShadowAtten = 1;
+#else
+#ifdef SHADOWS_SCREEN
+   half4 clipPos = TransformWorldToHClip(WorldPos);
+   half4 shadowCoord = ComputeScreenPos(clipPos);
+#else
+   half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+#endif
+   Light mainLight = GetMainLight(shadowCoord);
+   Direction = mainLight.direction;
+   Color = mainLight.color;
+   DistanceAtten = mainLight.distanceAttenuation;
+   ShadowAtten = mainLight.shadowAttenuation;
+#endif
 }
