3409de6f37cd80d99227474a50a5cac240a0c8fd
Kenzo-dewaegenaere <71668365+Kenzo-dewaegenaere@users.noreply.github.com>
2021-12-13 21:29:49 +0100

    #40  Added spells and voice controller

---
+++ PassionProject/Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class AudioController : MonoBehaviour
+{
+
+    private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
+    private KeywordRecognizer keywordRecognizer;
+
+   // private PlayerCombatController spells;
+    public GameObject spellcast;
+    void Start()
+    {
+
+        //spells = spellcast.GetComponent<PlayerCombatController>();
+        //Add keywords and set them to a function
+        keywordActions.Add("test", Test);
+
+
+        //put all the keywords in an array for easy acces, this way we don't need switch cases etc. 
+        keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void OnKeywordsRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Keyword:" + args.text);
+        keywordActions[args.text].Invoke();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void Test()
+    {
+        //spells.TestSpell();
+        Debug.Log("test has been said");
+    }
+}
---
+++ PassionProject/Assets/Scripts/Controllers/PlayerCombatController.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerCombatController : MonoBehaviour
+{
+
+    //get enemy health
+    public GameObject playerHealth;
+    //private NpcAiController currentEnemyHealth;
+
+    //declare range on enemy player has to be in, to set bool false or true
+    public float inAttackRange;
+    private bool playerInFightRange;
+
+    //enemy tag/layer
+    public LayerMask isEnemy;
+
+    //damage
+    public int damage = 10;
+
+    private int enemyHp;
+
+
+    public float cooldown = 1f; //seconds
+    private float lastAttackedAt = -9999f;
+    public float spellCooldown = 2f; 
+    void Start()
+    {
+        //get the health
+        //currentEnemyHealth = playerHealth.GetComponent<NpcAiController>();
+    }
+
+
+    private void Update()
+    {
+
+        //if in fight range set to true
+        playerInFightRange = Physics.CheckSphere(transform.position, inAttackRange, isEnemy);
+
+
+        
+        //if in fight range run code
+        if (playerInFightRange)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, inAttackRange, isEnemy);
+            foreach (var hitCollider in hitColliders)
+            {
+
+
+                GameObject enemy = hitCollider.gameObject;
+               
+                EnemyAiController enemyHealth = enemy.GetComponent<EnemyAiController>();
+
+
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                   
+                    if (Time.time > lastAttackedAt + cooldown)
+                    {
+                        Debug.Log("att");
+                        enemyHealth.DoDamage();
+                        Debug.Log(enemyHealth.currentEnemyHealth);
+                        lastAttackedAt = Time.time;
+                        
+                    }
+                   
+                }
+
+                if (Input.GetKeyDown(KeyCode.X))
+                {
+
+              
+                        Rigidbody rb = enemy.GetComponent<Rigidbody>();
+                        if (Time.time > lastAttackedAt + spellCooldown)
+                        {
+                            rb.isKinematic = false;
+                            rb.AddForce(transform.forward * 10, ForceMode.Impulse);
+                            enemyHealth.Kill();
+                            lastAttackedAt = Time.time;
+
+                        }
+                    
+
+                   
+
+                }
+
+
+                
+
+            }
+        }
+        else
+        {
+            //if in not fight range
+            //Debug.Log("not attackable");
+        }
+
+
+
+    }
+
+    public void TestSpell()
+    {
+
+        playerInFightRange = Physics.CheckSphere(transform.position, inAttackRange, isEnemy);
+
+        if (playerInFightRange)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, inAttackRange, isEnemy);
+            foreach (var hitCollider in hitColliders)
+            {
+
+                GameObject enemy = hitCollider.gameObject;
+                EnemyAiController enemyHealth = enemy.GetComponent<EnemyAiController>();
+
+                    Rigidbody rb = enemy.GetComponent<Rigidbody>();
+                    if (Time.time > lastAttackedAt + spellCooldown)
+                    {
+                        rb.isKinematic = false;
+                        rb.AddForce(transform.forward * 10, ForceMode.Impulse);
+                        enemyHealth.Kill();
+                        lastAttackedAt = Time.time;
+
+                    }
+            }
+        }
+        else
+        {
+            //if in not fight range
+            //Debug.Log("not attackable");
+        }
+    }
+
+}
+
+
